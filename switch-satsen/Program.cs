c84044484bd7d2c6def59eab3d06ba564b7f854b
linneasellerholm <linneasm@vgy.se>
2016-09-09 15:15:38 +0200

    övning 1

--- switch-satsen/Program.cs
+++ switch-satsen/Program.cs
@@ -10,7 +10,26 @@ namespace switch_satsen
     {
         static void Main(string[] args)
         {
-
+            //Övning 1
+            Console.WriteLine("Mata in ett heltal");
+            int tal = int.Parse(Console.ReadLine());
+            switch (tal)
+            {
+                case 1:
+                    Console.WriteLine("Du matade in alternativ 1!");
+                    break;
+                case 2:
+                    Console.WriteLine("Du matade in alternativ 2!");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Alternativ 3 och 4 är samma!");
+                    break;
+                default:
+                    Console.WriteLine("Ogiltigt alternativ!");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
